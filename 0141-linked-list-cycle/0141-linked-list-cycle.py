0449b52a224763222d7fdb824595f85f18d6277f
Muhammad Hasnain Shabbir <83360044+mhshabbir@users.noreply.github.com>
2023-10-04 00:07:01 -0400

    Time: 59 ms (48.38%), Space: 20.6 MB (43.3%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,12 +6,22 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        hashList = {}
-        while head:
-            if head in hashList and hashList[head] > 1:
+    #     hashList = {}
+    #     while head:
+    #         if head in hashList and hashList[head] > 1:
+    #             return True
+    #         else:
+    #             hashList[head] = 1 + hashList.get(head,0)
+    #         head = head.next
+        
+    #     return False
+
+        slow = head
+        fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
                 return True
-            else:
-                hashList[head] = 1 + hashList.get(head,0)
-            head = head.next
         
         return False
