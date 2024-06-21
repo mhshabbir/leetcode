97fe6dcfdb59b662954a0b65b13ac486726d2fc9
Muhammad Hasnain Shabbir <83360044+mhshabbir@users.noreply.github.com>
2024-06-21 14:26:16 -0400

    Time: 49 ms (6.77%), Space: 16.7 MB (6.95%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
@@ -6,22 +6,45 @@
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         """
-        iterate in the list and 
-        head = [1,1,2]
-                ^ ^ 
+        U: Remove dups that comes after. sorted list
+        time and space complexities
+        can an empty list be in an input?
+        Is it only numbers in the list?
+
+        M:
+        Multiple passing
+
+        P:
+        while P2:
+            check if p2.val == p1.val:
+                temp = p2.next
+                p1.next = p2.next
+                p2 = temp
+
+            else:
+                p1 = p1.next
+                p2 = p2.next
+
+                p2    T
+            1 -> 1 -> 2 -> N
+            p1
+        I
+        R
+        E
         """
-        if head == None or head.next == None:
+
+        if head == None:
             return head
 
-        ptr1 = head
-        ptr2 = head.next
-
-        while ptr1 and ptr2:
-            if ptr1.val == ptr2.val:
-                # temp = ptr2
-                ptr1.next = ptr2.next
-                ptr2 = ptr1.next
+        p1 = head
+        p2 = head.next
+        while p2:
+            print(p2.val)
+            if p2.val == p1.val:
+                p1.next = p2.next
+                p2 = p2.next
             else:
-                ptr1 = ptr2
-                ptr2 = ptr2.next
+                p1 = p1.next
+                p2 = p2.next
+        
         return head
