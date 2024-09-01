f16afe30a014fdefa39c795330ad7335eae4e720
Muhammad Hasnain Shabbir <83360044+mhshabbir@users.noreply.github.com>
2024-09-01 13:51:47 -0500

    Time: 192 ms (5.48%), Space: 20.9 MB (15.06%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -2,17 +2,16 @@ class Solution:
     def reverseString(self, s: List[str]) -> None:
         """
         Do not return anything, modify s in-place instead.
-        Approach 1: to assign a pointer in the front and a pointer in the back
-        use a temp to swap the ptrs
         """
 
-        l = 0
-        r = len(s) - 1
+        l = 0 
+        r = len(s)-1
         while l < r:
             temp = s[l]
+            print(temp)
             s[l] = s[r]
+            print(s[l], s[r])
             s[r] = temp
             l += 1
             r -= 1
-         
         
