0fa178a79828729acd5e6ef008f621b7462185ea
Muhammad Hasnain Shabbir <83360044+mhshabbir@users.noreply.github.com>
2024-09-09 22:56:18 -0500

    Time: 50 ms (94.34%), Space: 17.8 MB (10.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,34 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # """
-        #     U:
-        #     return the indices of elements which adds up to T
 
-        #     M:
-        #     using Hashmap to record the element
-        #     2 For loops
-
-        #     P:
-        #     hashmap = { element : index }
-        #     Target = 9
-        #     [2,7,11,15]
-        #      check if the compliment exist
-            
-        #      ^ 
-        #      { 2 : 0 , }
-        #     saving the compliment to the hashmaps
-
-        #     I: 
-        #     R:
-        # """
-        targetMap = {}
-        #  0 , 2
-        for i,n in enumerate(nums):
-            # 9 - 7 = 2
-            if (target - n) in targetMap:
-                index = targetMap[(target - n)]
-                return [index, i]
+        hashset = {}
+        for n,i in enumerate(nums):
+            if (target - i) in hashset:
+                return [hashset[target-i], n]
             else:
-                targetMap[n] = i
-
-            
+                hashset[i] = n
+        
