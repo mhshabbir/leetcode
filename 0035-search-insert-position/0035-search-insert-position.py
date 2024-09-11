86a618498a97d343db7ec6fcc3ecdbaf9e1d10b9
Muhammad Hasnain Shabbir <83360044+mhshabbir@users.noreply.github.com>
2024-09-11 11:03:01 -0500

    Time: 53 ms (30.15%), Space: 17.3 MB (12.4%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,25 +1,41 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         """
-        l = 0
-        r = 0
-        m = 0
+        Goal:
+        Return the index if target is found
+        if not:
+            return the sorted index where the target is suppose to go.
+
+        nums: [1, 3, 5, 6] T: 2
+        return 1
+
+        run time: log n
+                   l
+        [-1, 0, 1, 3, 5, 6]    t = 2
+                   ^ 
+                r
+
+        l = 3
+        r = 3
+        m = 3
         """
-        #           m
-        # # nums = [1,3,5,6,7,8,9,10]
-        #           l 
-        #           r
-        l, r = 0, len(nums)-1
-        while l<=r:
-            m = (r+l)//2
-            if target > nums[m]:
-                l = m + 1
-            elif target < nums[m]:
-                r = m - 1
+
+        # initial l, r
+        l = 0
+        r = len(nums) - 1
+
+        while l <= r:
+            mid = (r+l)//2
+            if target < nums[mid]:
+                r = mid - 1
+            
+            elif target > nums[mid]:
+                l = mid + 1
+                
             else:
-                return m
-        if target > nums[m]:
-            return m + 1
+                return mid
+        
+        if target > nums[mid]:
+            return mid + 1
         else:
-            return m 
-                 
+            return mid
