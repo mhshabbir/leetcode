7c67314ddb91cbee2e99493cae52b170dda2664d
Muhammad Hasnain Shabbir <83360044+mhshabbir@users.noreply.github.com>
2024-09-26 13:34:48 -0400

    Time: 673 ms (8.21%), Space: 16.5 MB (66.49%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,16 +1,15 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
         """
-         n = 2:
-         [1, 2, 2]
-
+        
         """
-        num = n
-        for i in range(1, n+1):
-            if i <= num:
-                num -= i
-                if i == n:
-                    return i
+        stairs = 0
+        for i in range(1,n+1):
+            if n - i < 0:
+                return stairs
             else:
-                return i - 1
+                stairs += 1
+                n -= i
+            
+        return stairs
         
