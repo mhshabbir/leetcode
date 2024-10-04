67d1b8c0e680cd412fccc6dabfe3b1544acf36a3
Muhammad Hasnain Shabbir <83360044+mhshabbir@users.noreply.github.com>
2024-10-04 13:58:14 -0400

    Time: 45 ms (59.77%), Space: 17.1 MB (25.32%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,40 +1,34 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        #[[1,3,5,7],[10,11,16,20],[23,30,34,60]]
-        #    L^                 ^M            ^R
-        
-        # [[1,3,5,7],[10,11,16,20]]
-        #  L^      ^M           ^R
-
-        # [1,3,5,7]
-        #  L^ M   R
-
-        # for row in matrix:
+        """
+                        M              R                            
+                       V      v               
+        [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+                    ^          
+             L
+        """
         left = 0
-        ileft = 0
-        right = len(matrix)-1
-        iright = len(matrix[right])-1
-
-    
-        # print(matrix[left][iLeft]) 
-        # print(matrix[right][iRight])    
-        # print(matrix[mid][iMid]) 
-        
-
+        right = len(matrix) - 1
         while left <= right:
-            mid=(left+right)//2
-            if target >= matrix[mid][ileft] and target <= matrix[mid][iright]:
-                while ileft <= iright:
-                    imid=(ileft + iright)//2
-                    if matrix[mid][imid] == target:
+            mid = (right+left)//2
+            innerLeft = 0
+            innerRight = len(matrix[mid]) - 1
+            while innerLeft <= innerRight:
+                innerMid = (innerLeft + innerRight)//2
+                if target >= matrix[mid][innerLeft] and target <= matrix[mid][innerRight]:
+                    if target < matrix[mid][innerMid]:
+                        innerRight = innerMid - 1
+                    elif target > matrix[mid][innerMid]:
+                        innerLeft = innerMid + 1
+                    else:
                         return True
-                    elif target > matrix[mid][imid]:
-                        ileft = imid + 1
-                    elif target < matrix[mid][imid]:
-                        iright = imid -1
-            elif target < matrix[mid][ileft]:
-                right = mid - 1
-            elif target > matrix[mid][iright]:
-                left = mid + 1
+                elif target < matrix[mid][innerLeft]:
+                    right = mid - 1
+                    break
+                elif target > matrix[mid][innerRight]:
+                    left = mid + 1
+                    break
         return False
+                    
+                    
             
