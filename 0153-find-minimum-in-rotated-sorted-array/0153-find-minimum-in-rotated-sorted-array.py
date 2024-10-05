4eca2eb36a0bdba6341ed15eb570acd038dbfd9b
Muhammad Hasnain Shabbir <83360044+mhshabbir@users.noreply.github.com>
2024-10-05 15:23:53 -0400

    Time: 46 ms (32.36%), Space: 16.8 MB (82.35%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,49 +1,26 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        # left = 0
-        # right = len(nums) -1
-        # mid = (left + right)//2
-        # while left <= right:
-        #     print(nums[mid-1],nums[mid],nums[mid+1])
-        #     if nums[mid+1] < nums[mid] and nums[mid+1] < nums[mid-1]:
-        #         left = mid + 1
-        #         mid = (left + right)//2
-        #     elif nums[mid-1] < nums[mid] and nums[mid+1] > nums[mid-1]:
-        #         right = mid - 1
-        #         mid = (left + right)//2
-        #     else:
-        #         return nums[mid]    
-        # left = 0
-        # right = len(nums) -1
-        # mid = (left + right)//2
-        # res = 0
-        # while left <= right:
-        #     if nums[mid] >= nums[left]:
-        #         res = nums[mid]
-        #         left = mid + 1
-        #         mid = (left + right)//2
-        #     else:
-        #         res = nums[mid]
-        #         right = mid - 1
-        #         mid = (left + right)//2
-        #     res = min(nums[mid],res)
-
-        # return res
-        start , end = 0, len(nums) - 1 
-        curr_min = float("inf")
+        """
+                        R
+                    M
+                0,1,2,3,4
+        nums = [3,4,5,1,2]
+                      L
         
-        while start  <  end :
-            mid = (start + end ) // 2
-            curr_min = min(curr_min,nums[mid])
-            
-            # right has the min 
-            if nums[mid] > nums[end]:
-                start = mid + 1
-                
-            # left has the  min 
+        if my m is less than R: 
+            smallest value is in the left side
+        
+        if my m is bigger than R than means the smaller value is in right side
+        """
+
+        left, right = 0, len(nums) - 1
+        res = float('inf')
+        while left <= right:
+            mid = left + (right - left)//2
+            # print(left, mid, right)
+            if nums[mid] <= nums[right]:
+                right = mid - 1
+                res = min(nums[mid], res)
             else:
-                end = mid - 1 
-                
-        return min(curr_min,nums[start])
-
-
+                left = mid + 1
+        return res
