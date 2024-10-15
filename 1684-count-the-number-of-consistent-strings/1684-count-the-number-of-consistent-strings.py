317fb56069452b8f66b98548effa370298bea300
Muhammad Hasnain Shabbir <83360044+mhshabbir@users.noreply.github.com>
2024-10-14 20:50:04 -0400

    Time: 207 ms (70.7%), Space: 18.5 MB (35.27%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.py
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.py
@@ -4,7 +4,10 @@ class Solution:
         print(allowedSet)
         res = 0
         for word in words:
-            wordSet = set(word)
-            if len(wordSet - allowedSet) == 0:
+            consistant = True
+            for char in word:
+                if char not in allowedSet:
+                   consistant = False
+            if consistant:
                 res += 1
         return res
