e5cd5404f436ffa796756017e2e65f4bc7a1f57f
Muhammad Hasnain Shabbir <83360044+mhshabbir@users.noreply.github.com>
2024-10-22 18:25:43 -0400

    Time: 7 ms (94.31%), Space: 16.7 MB (8%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,11 +1,21 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        """
-            step 1: remove the element from the list
-            return the remaining elements
+        #                 V
+        # nums = [2,2,2,3]
+        #             ^
+        # left = 0
+        # right = 0
+        # while right < len(nums):
+        #     if nums[left] == val or nums[right] == val:
+        #         while right < len(nums) - 1 and nums[right] == val:
+        #             right += 1
+        #         nums[left] = nums[right]
+        #     else:
+        #         nums[left] = nums[right]
 
-        """
-
+        #     left += 1
+        #     right += 1
+        # return left + 1
         for i in range(len(nums)-1,-1, -1):
             if nums[i] == val:
                 nums.remove(nums[i])
