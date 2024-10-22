345ed6891df9ff2fd3281704df5315ebe637fa61
Muhammad Hasnain Shabbir <83360044+mhshabbir@users.noreply.github.com>
2024-10-21 21:46:46 -0400

    Time: 4 ms (98.83%), Space: 18.4 MB (8.09%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,22 +1,5 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        """
-        Solution 1:
-        store all the elements in hashmap and count using iteration. 
-        check if the count if more than n/2 after adding it to the hash
-        """
-        # countHash = {}
-
-        # for i in nums:
-        #     countHash[i] = 1 + countHash.get(i,0)
-        #     if countHash[i] > len(nums)//2:
-        #         return i
-
-        """
-        BOYER MOORE Algorithm
-        Because the majority element always appears more than half the elements in the array, count the number 
-        of element and deduct the non majority element from it
-        """
         count = 0
         res = 0
         for n in nums:
@@ -25,6 +8,3 @@ class Solution:
             
             count += (1 if n == res else -1)
         return res
-
-
-            
