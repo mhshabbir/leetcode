37d6e86476174f2df1b900cd53622c2334862003
Muhammad Hasnain Shabbir <83360044+mhshabbir@users.noreply.github.com>
2024-10-21 21:36:05 -0400

    Time: 0 ms (100%), Space: 16.5 MB (65.42%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,76 +1,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # """
-        #     Brute Force: 
-        # """
-        # stack = []
-        # roundB = 0
-        # curlyB = 0  
-        # squareB = 0
-        # for bracket in s:
-        #     if bracket == "(":
-        #         roundB += 1
-        #     elif bracket == ")":
-        #         roundB -= 1
-        #     elif bracket == "{":
-        #         curlyB += 1
-        #     elif bracket == "}":
-        #         curlyB -= 1
-        #     elif bracket == "[":
-        #         squareB += 1
-        #     elif bracket == "]":
-        #         squareB -= 1
-
-        # if roundB < 0 or curlyB < 0 or squareB < 0:
-        #         return False
-            
-        # if roundB + curlyB + squareB == 0:
-        #     return True
-        # else:
-        #     return False
-
-        # stack = []
-        # hashmap = {")":"(", "]":"[", "}":"{"}
-        # for char in s:
-        #     if char == "(" or char == "{" or char == "[":
-        #         stack.append(char)
-        #     elif char in hashmap:
-        #         if stack and stack[-1] == hashmap[char]:
-        #             stack.pop()
-        #     else:
-        #         return False
-
-        # return True if stack else False
-
-        Map = {")": "(", "]": "[", "}": "{"}
         stack = []
-
-        for c in s:
-            if c not in Map:
-                stack.append(c)
-                continue
-            if not stack or stack[-1] != Map[c]:
-                return False
-            stack.pop()
-
-        return not stack 
-            
-                
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-            
+        for bracket in s:
+            if bracket == "[" or bracket == "(" or bracket == "{":
+                stack.append(bracket)
+            else:
+                if (stack and (bracket == "]" and stack[-1] == "[" or
+                    bracket == "}" and stack[-1] == "{" or
+                    bracket == ")" and stack[-1] == "(")):
+                    stack.pop()
+                else:
+                    return False
+        return True if not stack else False
