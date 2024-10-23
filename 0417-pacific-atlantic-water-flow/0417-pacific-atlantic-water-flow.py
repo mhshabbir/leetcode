e61c41524b86aff8b02ae0b21740490ae2ca551e
Muhammad Hasnain Shabbir <83360044+mhshabbir@users.noreply.github.com>
2024-10-23 19:14:48 -0400

    Time: 35 ms (96.8%), Space: 18.4 MB (26.72%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -15,12 +15,7 @@ class Solution:
         #     for dr,dc in direction:
         #         dfs(r+dr, c+dc, visit, heights[r][c])
 
-        # for c in range(COL):
-        #     dfs(0, c, pac, heights[0][c])
-        #     dfs(ROW - 1, c, atl, heights[ROW - 1][c])
-        # for r in range(ROW):
-        #     dfs(r, 0, pac, heights[r][0])
-        #     dfs(r, COL - 1, atl, heights[r][COL - 1])
+       
         
         # res = []
         # for r in range(ROW):
@@ -31,29 +26,24 @@ class Solution:
         ROWS, COLS = len(heights), len(heights[0])
         pac, atl = set(), set()
 
-        def dfs(r, c, visit, prevHeight):
-            if (
-                (r, c) in visit
+        def dfs(r,c, visit, preHeight):
+            if ((r,c) in visit 
                 or r < 0
                 or c < 0
-                or r == ROWS
-                or c == COLS
-                or heights[r][c] < prevHeight
-            ):
+                or r == ROWS or c == COLS 
+                or heights[r][c] < preHeight):
                 return
-            visit.add((r, c))
-            dfs(r + 1, c, visit, heights[r][c])
-            dfs(r - 1, c, visit, heights[r][c])
-            dfs(r, c + 1, visit, heights[r][c])
-            dfs(r, c - 1, visit, heights[r][c])
+            visit.add((r,c))
+            direction = [[1,0],[-1,0],[0,1],[0,-1]]
+            for dr,dc in direction:
+                dfs(r+dr, c+dc, visit, heights[r][c])
 
         for c in range(COLS):
             dfs(0, c, pac, heights[0][c])
             dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
-
         for r in range(ROWS):
             dfs(r, 0, pac, heights[r][0])
-            dfs(r, COLS - 1, atl, heights[r][COLS - 1])
+            dfs(r, COLS - 1, atl, heights[r][COLS - 1]) 
 
         res = []
         for r in range(ROWS):
