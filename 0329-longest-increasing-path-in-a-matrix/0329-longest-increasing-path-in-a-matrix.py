95719e47ebfe4dd14ccb2630111686157fa4864d
Muhammad Hasnain Shabbir <83360044+mhshabbir@users.noreply.github.com>
2024-11-03 16:30:46 -0800

    Time: 167 ms (52.11%), Space: 21.5 MB (29.43%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -7,12 +7,10 @@ class Solution:
 
         def dfs(row, col, prev_cell):
             if (row < 0 or col < 0 or row == ROWS or col == COLS
-                or (row,col) in visited or matrix[row][col] <= prev_cell):
+                or matrix[row][col] <= prev_cell):
                 return 0
             if (row, col) in LIP:
                 return LIP[(row,col)]
-            
-            visited.add((row, col))
 
             res1 = dfs(row+1, col, matrix[row][col])
             res2 = dfs(row-1, col, matrix[row][col])
@@ -20,7 +18,6 @@ class Solution:
             res4 = dfs(row, col-1, matrix[row][col])
 
             longest = max(1 + res1, 1 + res2, 1 + res3, 1 + res4)
-            visited.remove((row, col))
             LIP[(row,col)] = longest
             
             return longest
