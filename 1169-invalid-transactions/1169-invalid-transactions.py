6fffe1e58451b39c3526e310087bf5853760e6e7
Muhammad Hasnain Shabbir <83360044+mhshabbir@users.noreply.github.com>
2024-12-04 05:37:09 -0800

    Time: 55 ms (28.58%), Space: 18 MB (10.92%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.py
+++ 1169-invalid-transactions/1169-invalid-transactions.py
@@ -1,28 +1,36 @@
-from typing import List
-from collections import defaultdict
-
 class Solution:
-    def invalidTransactions(self, trans: List[str]) -> List[str]:
-        hmap = {}
-        invalid_indices = set()
-        
-        for i, t in enumerate(trans):
-            name, time, amount, city = t.split(',')
-            if name not in hmap:
-                hmap[name] = []
-            hmap[name].append((i, int(time), int(amount), city))
-        print(hmap)
+    def invalidTransactions(self, transactions: List[str]) -> List[str]:
+        """
+        make a hmap and store all transactions
 
-        
-        for name, transactions in hmap.items():
-            for i, (idx1, time1, amount1, city1) in enumerate(transactions):
-                print(idx1)
-                if amount1 > 1000:
-                    invalid_indices.add(idx1)
-                    
-                for j, (idx2, time2, amount2, city2) in enumerate(transactions):
-                    if i != j and abs(time1 - time2) <= 60 and city1 != city2:
-                        invalid_indices.add(idx1)
-                        invalid_indices.add(idx2)
+        iterate through all transactions and find the one which is invalid
+
+        """
+        hmap = {}
+        invalidTransaction = set()
+        invalidOutput = []
+        for i, t in enumerate(transactions):
+            name, time, amt, city = t.split(',')
+
+            if name not in hmap:
+                hmap[name] = [[i, int(time), int(amt), city]]
+            else:
+                hmap[name].append([i, int(time), int(amt), city])
             
-        return [trans[i] for i in invalid_indices]
+        for name, transaction in hmap.items():
+            for transA in hmap[name]:
+                i, time, amt, city = transA
+
+                if amt > 1000:
+                    invalidTransaction.add(i)
+                
+                for transB in hmap[name]:
+                    j, timeJ, amtJ, cityJ = transB
+
+                    if i != j and abs(time - timeJ) <= 60 and city != cityJ:
+                        invalidTransaction.add(i)
+                        invalidTransaction.add(j)
+        for idx in invalidTransaction:
+            invalidOutput.append(transactions[idx])
+        
+        return invalidOutput
