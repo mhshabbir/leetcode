af6e3b38341a1920c89b6eb8da1e33eecc84a36f
Muhammad Hasnain Shabbir <83360044+mhshabbir@users.noreply.github.com>
2025-01-26 19:16:49 -0500

    Time: 0 ms (100%), Space: 17.8 MB (49.28%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,21 +3,29 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        """                V
+        nums1 = [1,2,2,3,5,6]
+                   ^   
+                   m
+        nums2 = [2,5,6]
+                   ^
+                   n
+        """
         cur = len(nums1) - 1
-        n -= 1
-        m -= 1
-        while n > -1 and m > -1:
-            if nums2[n] > nums1[m]:
-                nums1[cur] = nums2[n]
-                n -= 1
-                cur -= 1
-            else:
-                nums1[cur] = nums1[m]
-                m -= 1
-                cur -= 1
+        m = m - 1
+        n = n - 1
 
+        while n >= 0 and m >= 0:
+            if nums1[m] >= nums2[n]:
+                nums1[cur] = nums1[m]
+                m = m - 1
+            else:
+                nums1[cur] = nums2[n]
+                n = n - 1
+            cur = cur - 1
+        
         while n > -1:
             nums1[cur] = nums2[n]
-            n -= 1
-            cur -= 1
+            n = n - 1
+            cur = cur - 1
             
