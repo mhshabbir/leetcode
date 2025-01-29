86d104de59fe1259a04165dfd578ba373d2d2995
Muhammad Hasnain Shabbir <83360044+mhshabbir@users.noreply.github.com>
2025-01-28 20:25:41 -0500

    Time: 266 ms (36.97%), Space: 28.8 MB (7.56%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,33 +1,41 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        """
-        Input: grid = [
-                        ["1","1","1","1","0"],
-                        ["1","1","0","1","0"],
-                        ["1","1","0","0","0"],
-                        ["0","0","0","0","0"],
-                        ]
-                        [[1, 0],
-                         [1, 0]]
-        """
-        def dfs(grid, r, c, visited):
-            print(r,c)
-            if ((r < 0 or c < 0) 
-                    or (r == len(grid) or c == len(grid[0]))
-                    or grid[r][c] == "0" 
-                    or (r,c) in visited):
-                visited.add((r,c))
-                return
-            visited.add((r,c))
-            dfs(grid, r+1, c, visited)
-            dfs(grid, r-1, c, visited)
-            dfs(grid, r, c+1, visited)
-            dfs(grid, r, c-1, visited)
-        numberOfIsland = 0
+        ROWS, COLS = len(grid), len(grid[0])
         visited = set()
-        for row in range(len(grid)):
-            for col in range(len(grid[0])):
-                if (row,col) not in visited and grid[row][col] == "1":
-                    numberOfIsland += 1
-                    dfs(grid, row, col, visited)
-        return numberOfIsland
+        
+        def dfs(row, col):
+            if (row < 0 or col < 0 or row >= ROWS or col >= COLS or 
+            (row, col) in visited or grid[row][col] == "0"):
+                return
+            
+            visited.add((row,col))
+
+            dfs(row + 1, col)
+            dfs(row - 1, col)
+            dfs(row, col + 1)
+            dfs(row, col - 1)
+
+        # def bfs(r,c):
+        #     q = collections.deque()
+        #     visited.add((r, c))
+        #     q.append((r, c))
+
+        #     while q:
+        #         row, col = q.popleft()
+        #         directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+                
+        #         for dr, dc in directions:
+        #             r, c = row + dr, col + dc
+        #             if (r in range(ROWS) and c in range(COLS) and (r,c) not in visited and grid[r][c] == "1"):
+        #                 q.append((r,c))
+        #                 visited.add((r,c))
+
+
+        count = 0
+        for row in range(ROWS):
+            for col in range(COLS):
+                if grid[row][col] == "1" and (row, col) not in visited:
+                    dfs(row, col)
+                    count += 1
+
+        return count
