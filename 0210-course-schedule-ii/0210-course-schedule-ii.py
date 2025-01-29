1ee79a77c0b9c6c8666ef11430e7d8d8ca470ae6
Muhammad Hasnain Shabbir <83360044+mhshabbir@users.noreply.github.com>
2025-01-28 23:07:03 -0500

    Time: 3 ms (78.28%), Space: 19.9 MB (10.54%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,10 +1,30 @@
+
+
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        # pathway = set()
-        # result = []
-        # courseMap = {c: [] for c in range(numCourses)}
-        # for crs, pre in prerequisites:
-        #     courseMap[crs].append(pre)
+
+        cycle = set()
+        visit = set()
+        output = []
+        courseMap = {c: [] for c in range(numCourses)}
+        for crs, pre in prerequisites:
+            courseMap[crs].append(pre)
+        
+
+        def dfs(crs):
+            if crs in cycle:
+                return False
+            if crs in visit:
+                return True
+
+            cycle.add(crs)
+            for pre in courseMap[crs]:
+                if dfs(pre) == False:
+                    return False
+            cycle.remove(crs)
+            visit.add(crs)
+            output.append(crs)
+            return True
 
         # def dfs(course):
         #     if course in pathway:
@@ -20,29 +40,6 @@ class Solution:
         #     courseMap[course] = []
         #     result.append(course)
         #     return True
-
-        
-        prereq = {c: [] for c in range(numCourses)}
-        for crs, pre in prerequisites:
-            prereq[crs].append(pre)
-
-        output = []
-        visit, cycle = set(), set()
-
-        def dfs(crs):
-            if crs in cycle:
-                return False
-            if crs in visit:
-                return True
-
-            cycle.add(crs)
-            for pre in prereq[crs]:
-                if dfs(pre) == False:
-                    return False
-            cycle.remove(crs)
-            visit.add(crs)
-            output.append(crs)
-            return True
 
         for course in range(numCourses):
             if not dfs(course): return []
