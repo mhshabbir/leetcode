b9fb5b98328d228544bfbb5d9879560352b68667
Muhammad Hasnain Shabbir <83360044+mhshabbir@users.noreply.github.com>
2025-03-03 23:17:50 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,20 +3,25 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        """                V
-        nums1 = [1,2,2,3,5,6]
-                   ^   
-                   m
-        nums2 = [2,5,6]
-                   ^
-                   n
+        """
+            [1,3,4,6]
+            never be [1,4,3,5]
+
+            nums1 = [1,2,3,0,0,0] insert nums2 [2,5,6] 
+            
+
+            [2,5,6]
+                 n
+
+            [1,2,3,0,0,6]
+                 m   i
         """
         cur = len(nums1) - 1
-        m = m - 1
         n = n - 1
+        m = m - 1
 
         while n >= 0 and m >= 0:
-            if nums1[m] >= nums2[n]:
+            if nums1[m] > nums2[n]:
                 nums1[cur] = nums1[m]
                 m = m - 1
             else:
@@ -24,8 +29,11 @@ class Solution:
                 n = n - 1
             cur = cur - 1
         
-        while n > -1:
+        while n >= 0:
             nums1[cur] = nums2[n]
             n = n - 1
-            cur = cur - 1
-            
+        
+        
+
+
+
